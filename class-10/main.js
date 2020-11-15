a06cc6e3a8d50f4b8be98e8027a3c60f21902d54
anis3139 <anis904692@gmail.com>
2020-11-16 00:00:05 +0600

    class-10

--- class-10/main.js
+++ class-10/main.js
@@ -1,11 +1,73 @@
-// alert("Hello Wold");
+var fname='Sarmin AKter'
+var district='Dhaka'
+var age=50
 
-var a=20;
-var b= 1971;
-var x = a+b;
-var Fname="Anichur";
-var Lname="Rahaman";
-var m="12";
-var n="10";
+console.log(name);
+console.log(name+' '+'Is Gentle Man and he lives in '+ district);
+console.log(name+" "+ "is "+ age +" years old");
+console.log(name+" "+"has 2 sons "+ district);
 
-console.log(m+n);
+
+var arrayName=['Karim Uddin', 'Rahim Uddin','Rohima Khatumn', 'Kamal Uddin', 'Husneara khatun', 50, 60, 50 , 70 , 510 ,450];
+
+console.log(arrayName.length);
+
+console.log(arrayName[arrayName.length-1]);
+
+
+
+// var i;
+// for(i=0; i<10; i++){
+//     console.log("I am Here");
+// }
+
+
+
+var newFanc=function(){
+
+}
+newFanc();
+
+
+
+function firstFunction(a,b, cb){
+    var sub=a*b
+    console.log(sub);
+
+}
+
+firstFunction(5,2, function() {
+    
+}) 
+
+firstFunction(30,2) 
+
+firstFunction(10,5) 
+
+// var age=18;
+
+// if(age<=18){
+//     console.log("No")
+// }else{
+//     console.log("yes")
+// }
+
+
+var user ="admin";
+var pass ="1234567sdfdgdfgjjj";
+
+if(user == "admin" && pass=="12345"){
+    console.log("Welcome");
+}else if(user == "admin" && pass=="123456"){
+    console.log("you are also welcome");
+}else if(user == "admin" && pass=="1234567"){
+    console.log("you are Another Person");
+}
+else{
+    console.log("wrong");
+}
+
+
+
+
+
